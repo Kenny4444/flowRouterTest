a3706891d8f71c0b4068ca768cd8de69e6982fb3
Kenny Votava <kvotava@gmail.com>
2016-05-24 14:37:44 -0700

    update

--- client/app.js
+++ client/app.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Grid } from 'react-bootstrap';
+// import React from 'react';
+// import { Grid } from 'react-bootstrap';
 
-export const App = React.createClass({
-  propTypes: {
-    children: React.PropTypes.element.isRequired,
-  },
-  render() {
-    return <div>
-      <Grid>
-        { this.props.children }
-      </Grid>
-    </div>;
-  },
-});
+// export const App = React.createClass({
+//   propTypes: {
+//     children: React.PropTypes.element.isRequired,
+//   },
+//   render() {
+//     return <div>
+//       <Grid>
+//         { this.props.children }
+//       </Grid>
+//     </div>;
+//   },
+// });
+
+import React from 'react';
+
+export const App = ({content}) => (
+  <div>
+    <h1>My App</h1>
+    <hr />
+    <div>{content}</div>
+  </div>
+);
