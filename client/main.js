650885209c16bbc85ac8e9e83764bec97e434b3e
Kenny Votava <kvotava@gmail.com>
2016-05-24 14:39:00 -0700

    update

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {mount} from 'react-mounter';
-import App from './app.js';
+import {App} from './app.js';
 import { Index } from './index.js';
 FlowRouter.route("/",{
   action(){
